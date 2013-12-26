124cea655ce8ba5102a1196800735c9dba1467b2
Nate Parrott <nateparro2t@gmail.com>
2013-12-26 00:56:17 -0500

    Initial Commit

---
+++ StreamKit/StreamKit.h
@@ -0,0 +1,13 @@
+//
+//  StreamKit.h
+//  StreamKit
+//
+//  Created by Nate Parrott on 12/26/13.
+//  Copyright (c) 2013 Nate Parrott. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StreamKit : NSObject
+
+@end
